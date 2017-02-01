d188bdef44df497a1245bb9d2f8d54d1a9c85e17
Nuutti <nuutti.luomala@outlook.com>
2017-02-02 00:36:41 +0200

    Flips given word(s) upside down

---
+++ plugins/fliptext/flip.py
@@ -0,0 +1,24 @@
+from modules.globals import Globals
+from modules.pluginbase import PluginBase
+from modules import upsidedown as upd
+
+class Plugin(PluginBase):
+    # plugin specific
+
+    def __init__(self):
+        self.type = PluginBase.PluginType.UNCORE
+        self.name = 'Fliptext'
+        t = PluginBase.Trigger()
+        t.add_event('on_message', 'upd', True, self.on_message)
+        self.trigger = t.functions
+        self.help = 'Flips a given string upside down'
+
+    async def on_message(self, message, trigger):
+        try:
+            msg = self.Command(message)
+            text = ' '.join(msg.parts[1:])
+            await Globals.disco.send_message(message.channel, upd.flip(text))
+        except Exception as e:
+            Globals.log.error(f'Could not flip and send the message: {str(e)}')
+            await Globals.disco.send_message(message.channel, 'Something went wrong')
+            return False
