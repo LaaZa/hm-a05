a1bf23ee8c39f78052c11d02f5424d6a88343483
LaaZa <jesse.laatu@gmail.com>
2017-05-16 08:03:20 +0300

    Fixed Flip plugin name

--- plugins/flip/flip.py
+++ plugins/flip/flip.py
@@ -2,12 +2,13 @@ from modules.globals import Globals
 from modules.pluginbase import PluginBase
 from modules import upsidedown as upd
 
+
 class Plugin(PluginBase):
     # plugin specific
 
     def __init__(self):
         self.type = PluginBase.PluginType.UNCORE
-        self.name = 'Fliptext'
+        self.name = 'Flip text'
         t = PluginBase.Trigger()
         t.add_event('on_message', 'upd', True, self.on_message)
         self.trigger = t.functions
@@ -18,6 +19,7 @@ class Plugin(PluginBase):
             msg = self.Command(message)
             text = ' '.join(msg.parts[1:])
             await Globals.disco.send_message(message.channel, upd.flip(text))
+            return True
         except Exception as e:
             Globals.log.error(f'Could not flip and send the message: {str(e)}')
             await Globals.disco.send_message(message.channel, 'Something went wrong')
