dcc4ac5643f0df180ac9611cad45ad598a60ee06
LaaZa <jesse.laatu@gmail.com>
2017-10-09 21:41:28 +0300

    Implemented permissions and polish to Dislike vote plugin.

--- plugins/dislikevote/dislikevote.py
+++ plugins/dislikevote/dislikevote.py
@@ -16,17 +16,25 @@ class Plugin(PluginBase):
         t.add_event('on_message', 'dislikevote', True, self.on_message)
         t.add_event('on_reaction_add', self.reaction_check, False, self.on_reaction_add)
         self.trigger = t.functions
-        self.help = 'Removes messages with enough certain reactions'
+        self.help = 'Removes messages with enough certain reactions. | set :emoji: limit | remove'
 
         self.subcommands = {
             'set': self.sub_set,
             'remove': self.sub_remove,
         }
 
-        # Guild: Channel: Emoji: 'limit': int
-        self.dislike_setting = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
+        # Channel: Emoji: 'limit': int
+        self.dislike_setting = defaultdict(lambda: defaultdict(dict))
 
     async def on_message(self, message, trigger):
+        if not (Globals.permissions.has_permission(message.author, Globals.permissions.PermissionLevel.admin) or Globals.permissions.has_discord_permissions(message.author, ('manage_messages',), message.channel)):
+            await message.author.send(f'You don\'t have rights to use dislikevote command on {message.guild.name} #{message.channel.name}')
+            return True
+
+        if not Globals.permissions.client_has_discord_permissions(('manage_messages',), message.channel):
+            await message.channel.send('I don\'t have rights to manage messages here.')
+            return True
+
         try:
             msg = self.Command(message)
             if message.guild:
@@ -41,17 +49,14 @@ class Plugin(PluginBase):
     async def dummy(self, m):
         pass
 
-    # TODO support more settings
     def reaction_check(self, reaction, user, **kwargs):
-        channels = self.dislike_setting.get(reaction.message.guild, dict())
-        emoji = channels.get(reaction.message.channel, dict())
-        if str(reaction.emoji) in emoji.keys():
+        emoji = self.dislike_setting.get(reaction.message.channel, dict())
+        if str(reaction.emoji) in emoji.keys() and not reaction.message.pinned:
             if reaction.count >= emoji.get(str(reaction.emoji), dict()).get('limit', -1):
                 return True
         return False
 
     async def on_reaction_add(self, reaction, user, **kwargs):
-        msg = reaction.message
         try:
             await reaction.message.delete()
             return True
@@ -59,30 +64,29 @@ class Plugin(PluginBase):
             Globals.log.error(f'No permission to delete messages: {str(e)}')
         return False
 
-    # TODO permissions and more settings
     async def sub_set(self, message):
         msg = Plugin.Command(message)
         try:
             emoji = msg.word(1)
             limit = msg.word(2)
             self.dislike_setting.update({
-                message.guild: {
-                    message.channel: {
-                        emoji: {
-                            'limit': int(limit)
-                        }
+                message.channel: {
+                    emoji: {
+                        'limit': int(limit)
                     }
                 }
             })
-        except Exception:
+            await message.channel.send(f'Set dislike vote on this channel to {limit}x {emoji}')
+        except Exception as e:
+            Globals.log.error(f'Could not set dislike vote: {str(e)}')
+            await message.channel.send('Could not set dislike vote.')
             return False
         return True
 
-    # TODO permissions
     async def sub_remove(self, message):
-        msg = Plugin.Command(message)
         try:
-            self.dislike_setting.get(message.guild, dict()).pop(message.channel)
+            self.dislike_setting.pop(message.channel)
+            await message.channel.send(f'Removed dislike vote on this channel.')
         except Exception:
-            return False
+            await message.channel.send(f'No dislike vote set on this channel.')
         return True
