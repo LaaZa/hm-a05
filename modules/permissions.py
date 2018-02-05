2e1a0dacef13f692ce8188c7a781037419680195
LaaZa <jesse.laatu@gmail.com>
2018-02-05 13:35:30 +0200

    Fixed client_has_discord_permissions in guildless context

--- modules/permissions.py
+++ modules/permissions.py
@@ -78,4 +78,8 @@ class Permissions:
         return member.guild_permissions.is_subset(permissions)
 
     def client_has_discord_permissions(self, permissions_tuple: tuple, channel):
-        return self.has_discord_permissions(channel.guild.me, permissions_tuple, channel=channel)
+        # try to get either Member object or ClientUser
+        try:
+            return self.has_discord_permissions(channel.guild.me, permissions_tuple, channel=channel)
+        except AttributeError:
+            return self.has_discord_permissions(Globals.disco.user, permissions_tuple, channel=channel)
