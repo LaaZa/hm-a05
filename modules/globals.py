168d38ec1f5e3622720aa75f6ae4a0ce8e1e8882
Jesse Laatu <jesse.laatu@gmail.com>
2022-01-11 10:56:06 +0200

    Initial implementation of SavedVar, requires varname package

--- modules/globals.py
+++ modules/globals.py
@@ -1,3 +1,8 @@
+import pickle
+import inspect
+from os import path, pardir
+from varname.helpers import register
+
 class Globals:
     config_data = None
     log = None
@@ -16,3 +21,48 @@ class Globals:
     @classmethod
     def channel_command_prefix(cls, channel):
         return cls.__channel_command_prefix.get(channel, cls.default_command_prefix)
+
+    @register
+    class SavedVar:
+
+        _picklefile = path.abspath(path.join(path.join(path.dirname(__file__), pardir), 'save.pickle'))
+        pickledata = {}
+        loaded = False
+
+        def __init__(self, var=None):
+
+            frame = inspect.currentframe()
+            frame = frame.f_back.f_back
+            code = frame.f_code
+            self._file = path.relpath(code.co_filename, path.join(path.dirname(__file__), pardir))
+            self._name = self.__varname__
+            self._data = var
+            if not self.loaded:
+                self._load()
+            if not self.loaded and f'{self._file}#{self._name}' not in self.pickledata.keys():
+                self.save()
+
+        def _load(self):
+            try:
+                with open(self._picklefile, 'rb') as pic:
+                    self.pickledata = pickle.load(pic)
+                self.loaded = True
+            except Exception:
+                pass
+
+        def save(self):
+            with open(self._picklefile, 'wb') as pic:
+                self.pickledata.update({f'{self._file}#{self._name}': self._data})
+                pickle.dump(self.pickledata, pic)
+
+        @property
+        def x(self):
+            return self.pickledata.get(f'{self._file}#{self._name}', None)
+
+        @x.setter
+        def x(self, val):
+            self._data = val
+            self.save()
+
+        def __repr__(self):
+            return self.x
